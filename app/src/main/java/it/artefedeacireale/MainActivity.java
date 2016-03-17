019c24779a4b992d13ac6245a77ff50d795da4bd
Davide Agostini <dsagostini@gmail.com>
2016-03-17 21:33:20 +0100

    add new material drawer and icon

--- app/src/main/java/it/artefedeacireale/MainActivity.java
+++ app/src/main/java/it/artefedeacireale/MainActivity.java
@@ -5,54 +5,88 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.util.ArrayList;
 
 import it.artefedeacireale.activities.ChurchListActivity;
+import it.artefedeacireale.activities.CreditsActivity;
 import it.artefedeacireale.adapters.ItineraryListAdapter;
 import it.artefedeacireale.api.models.Itinerary;
 import it.artefedeacireale.services.ItineraryService;
 import it.artefedeacireale.util.NetworkUtil;
 import it.artefedeacireale.util.RecyclerViewClickListener;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private Intent itineraryIntentService;
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
     private ItineraryListAdapter mItineraryListAdapter;
+    private DrawerBuilder drawerBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Itinerari");
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
         startDownloadItineraries();
+
+        final PrimaryDrawerItem itemItinerary = new PrimaryDrawerItem().withName(R.string.itinerari).withIcon(R.mipmap.ic_map_black_24dp).withSelectable(false);
+        final PrimaryDrawerItem itemMap = new PrimaryDrawerItem().withName(R.string.mappa).withIcon(R.mipmap.ic_place_black_24dp).withSelectable(false);
+        final PrimaryDrawerItem itemFotoVideo = new PrimaryDrawerItem().withName(R.string.foto_video).withIcon(R.mipmap.ic_photo_library_black_24dp).withSelectable(false);
+        final PrimaryDrawerItem itemEvent = new PrimaryDrawerItem().withName(R.string.eventi).withIcon(R.mipmap.ic_event_black_24dp).withSelectable(false);
+        final PrimaryDrawerItem itemInfo = new PrimaryDrawerItem().withName(R.string.info).withIcon(R.mipmap.ic_email_black_24dp).withSelectable(false);
+        final PrimaryDrawerItem itemCredits = new PrimaryDrawerItem().withName(R.string.credits).withIcon(R.mipmap.ic_info_outline_black_24dp).withSelectable(false);
+
+        drawerBuilder = new DrawerBuilder()
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .withActivity(this)
+                .withTranslucentStatusBar(true)
+                .withAccountHeader(new AccountHeaderBuilder()
+                                .withActivity(this)
+                                .withHeaderBackground(R.drawable.side_nav_bar)
+                                .build()
+                )
+                .withSavedInstance(savedInstanceState)
+                .withActionBarDrawerToggle(true)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem.equals(itemCredits)) {
+                            openCredits();
+                        }
+
+                        return false;
+                    }
+                });
+
+        drawerBuilder.addDrawerItems(
+                itemItinerary,
+                itemMap,
+                itemFotoVideo,
+                itemEvent,
+                itemInfo,
+                itemCredits
+        )
+                .withSelectedItem(0)
+                .build();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.list);
         mRecyclerView.setHasFixedSize(true);
@@ -77,20 +111,9 @@ public class MainActivity extends AppCompatActivity
 
             @Override
             public void onItemLongPress(View view, int position) {
-
             }
         }));
 
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -120,51 +143,9 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
+    public void openCredits() {
+        Intent intent = new Intent(this, CreditsActivity.class);
+        startActivity(intent);
     }
 
     private BroadcastReceiver itineraryReceiver = new BroadcastReceiver() {
@@ -174,4 +155,6 @@ public class MainActivity extends AppCompatActivity
             mItineraryListAdapter.setItineraries(itineraries);
         }
     };
+
+
 }
---
+++ app/src/main/java/it/artefedeacireale/activities/CreditsActivity.java
@@ -0,0 +1,37 @@
+package it.artefedeacireale.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import it.artefedeacireale.R;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("Credits");
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
